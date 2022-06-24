b0def140f0b77f50e2fc5d813b3b0ac8d516a4ed
Simon Lance <sl484@exeter.ac.uk>
2022-06-24 15:51:49 +0100

    Added basic page functionality

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get('/')
-async def home():
-    return {"message": "Hello World"}
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/user/{user_id}")
 async def user_profile(user_id: str):
     return {"user" : user_id}
+
+@app.get('/blogs')
+async def blogs(request: Request):
+    return templates.TemplateResponse("blogs/index.html", {"request": request})
